2b8acff24be57f97087afe2bea6eccb2bf48868c
tmurray <tmurray@wumpus-cave.net>
2020-03-02 19:08:24 -0600

    Don't export Authenticators by default

--- index.ts
+++ index.ts
@@ -1,2 +1,1 @@
 export * from './src/castellated';
-export * from './src/auth/plaintext';
--- test/plaintext.ts
+++ test/plaintext.ts
@@ -1,6 +1,6 @@
-import * as Castellated from '../index';
+import * as Auth from '../src/auth/plaintext';
 import * as Tap from 'tap';
 
-const plain = new Castellated.PlaintextAuth( "foobar" );
+const plain = new Auth.PlaintextAuth( "foobar" );
 Tap.ok( plain.isMatch( "foobar" ), "Password matches" );
 Tap.ok(! plain.isMatch( "barfoo" ), "Bad password doesn't match" );
